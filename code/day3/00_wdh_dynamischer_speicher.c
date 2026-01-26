fdfb636d86ebd70ecf765e799fd232144505efbb
Raphael <raphael.goetz.7@gmail.com>
2026-01-26 08:19:56 +0100

    ref: correct c formatting :)

--- code/day3/00_wdh_dynamischer_speicher.c
+++ code/day3/00_wdh_dynamischer_speicher.c
@@ -1,64 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct student 
-{
-	char *vorname;
-	char *nachname;
-	int mtr;
-}staticstudent, *dynamicstudent; /*staticstudent & dynamicstudent sind globale Variablen vom Typ student*/
+struct student {
+  char *vorname;
+  char *nachname;
+  int mtr;
+} staticstudent, *dynamicstudent; /*staticstudent & dynamicstudent sind globale
+                                     Variablen vom Typ student*/
 
+int main(void) {
+  /* ++++++++++++++++++++Verarbeiten von staticstudent++++++++++++++++++++++++*/
+  staticstudent.vorname = (char *)malloc(sizeof(char) * 32);
+  staticstudent.nachname = (char *)malloc(sizeof(char) * 32);
 
-int main(void)
-{
-	/* ++++++++++++++++++++Verarbeiten von staticstudent++++++++++++++++++++++++*/
-	staticstudent.vorname = (char*)malloc(sizeof(char)*32);
-	staticstudent.nachname = (char*)malloc(sizeof(char)*32);
+  strcpy(staticstudent.vorname, "Hans");
+  strcpy(staticstudent.nachname, "Mustermann");
+  staticstudent.mtr = 123;
 
-	strcpy(staticstudent.vorname, "Hans");
-	strcpy(staticstudent.nachname, "Mustermann");
-	staticstudent.mtr = 123;
+  /*Ausgabe*/
+  printf("%s %s %d", staticstudent.vorname, staticstudent.nachname,
+         staticstudent.mtr);
 
-	/*Ausgabe*/
-	printf("%s %s %d", staticstudent.vorname, staticstudent.nachname, staticstudent.mtr);
+  /*Inneren Speicher freigeben*/
+  free(staticstudent.vorname);
+  free(staticstudent.nachname);
+  staticstudent.vorname = NULL;
+  staticstudent.nachname = NULL;
 
-	/*Inneren Speicher freigeben*/
-	free(staticstudent.vorname);
-	free(staticstudent.nachname);
-	staticstudent.vorname = NULL;
-	staticstudent.nachname = NULL;
+  /* ++++++++++++++++++++Verarbeiten von
+   * dynamicstudent++++++++++++++++++++++++*/
+  dynamicstudent = (struct student *)malloc(sizeof(struct student));
+  dynamicstudent->vorname = (char *)malloc(sizeof(char) * 32);
 
+  strcpy(dynamicstudent->vorname, "Hans");
+  strcpy(dynamicstudent->nachname, "Mustermann");
+  dynamicstudent->mtr = 123;
 
-	/* ++++++++++++++++++++Verarbeiten von dynamicstudent++++++++++++++++++++++++*/
-	dynamicstudent = (struct student*)malloc(sizeof(struct student));
-	dynamicstudent->vorname = (char*)malloc(sizeof(char)*32);
+  /*Ausgabe*/
+  printf("%s %s %d", dynamicstudent->vorname, dynamicstudent->nachname,
+         dynamicstudent->mtr);
 
-	strcpy(dynamicstudent->vorname, "Hans");
-	strcpy(dynamicstudent->nachname, "Mustermann");
-	dynamicstudent->mtr = 123;
+  /*Freigabe, erst innere Speicher, dann struct selbst*/
+  free(dynamicstudent->vorname);
+  free(dynamicstudent->nachname);
 
-	/*Ausgabe*/
-	printf("%s %s %d", dynamicstudent->vorname, dynamicstudent->nachname, dynamicstudent->mtr);
-	
-	/*Freigabe, erst innere Speicher, dann struct selbst*/
-	free(dynamicstudent->vorname);
-	free(dynamicstudent->nachname);
+  free(dynamicstudent);
+  dynamicstudent = NULL; /*UseAfterFree verhindern*/
 
-	free(dynamicstudent);
-	dynamicstudent = NULL; /*UseAfterFree verhindern*/
-
-	return 0;
+  return 0;
 }
 
-
 /*
- *	staticstudent ist im statischen Bereich (Stack) 20Byte groß. 
+ *	staticstudent ist im statischen Bereich (Stack) 20Byte groß.
  *	8Byte Pointer Vorname + 8Byte Pointer Vorname + 4Byte int mtr
  *	im dynamischen Bereich (Heap) 2*32Byte für malloc Vor- und Nachname
  *
  *	dynamicstudent ist im statischen Bereich 8Byte groß (weil Pointer)
- *	im dynamischen Bereich (Heap) 8Byte Pointer Vorname + 8Byte Pointer Vorname + 4Byte int mtr + 2*32Byte für malloc Vor- und Nachname
+ *	im dynamischen Bereich (Heap) 8Byte Pointer Vorname + 8Byte Pointer
+ * Vorname + 4Byte int mtr + 2*32Byte für malloc Vor- und Nachname
  *
  *
  *
--- code/day3/02_liste_easy.c
+++ code/day3/02_liste_easy.c
@@ -1,114 +1,98 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /*Strukturen*/
-struct primzahlen
-{
-	long primzahl;
-	struct primzahlen *next;
+struct primzahlen {
+  long primzahl;
+  struct primzahlen *next;
 };
 
 /*Globale Variablen*/
 struct primzahlen *firstElement = NULL;
 struct primzahlen *lastElement = NULL;
 
-struct primzahlen *NeuePrimzahl(long p)
-{
-	struct primzahlen *tempPrimzahl = (struct primzahlen*)malloc(sizeof(struct primzahlen));
-	tempPrimzahl->primzahl = p;
-	tempPrimzahl->next = NULL;
-	return tempPrimzahl;
+struct primzahlen *NeuePrimzahl(long p) {
+  struct primzahlen *tempPrimzahl =
+      (struct primzahlen *)malloc(sizeof(struct primzahlen));
+  tempPrimzahl->primzahl = p;
+  tempPrimzahl->next = NULL;
+  return tempPrimzahl;
 }
 
-void AddPrimzahl(long p)
-{
-	struct primzahlen *tempPrimzahl = NeuePrimzahl(p);
-	if(firstElement == NULL)
-		firstElement = lastElement = tempPrimzahl;
-	else
-	{
-		/* neues Element hinter lastElement anhängen */
-		lastElement->next = tempPrimzahl;
-		lastElement = tempPrimzahl;
-	}
+void AddPrimzahl(long p) {
+  struct primzahlen *tempPrimzahl = NeuePrimzahl(p);
+  if (firstElement == NULL)
+    firstElement = lastElement = tempPrimzahl;
+  else {
+    /* neues Element hinter lastElement anhängen */
+    lastElement->next = tempPrimzahl;
+    lastElement = tempPrimzahl;
+  }
 }
 
-void List()
-{
-	struct primzahlen *tempElement = firstElement;
-	int count = 1;
+void List() {
+  struct primzahlen *tempElement = firstElement;
+  int count = 1;
 
-	while(tempElement != NULL)
-	{
-		printf("Zahl[%d] ist [%ld]\n", count, tempElement->primzahl);
-		/*zum nächsten Element springen*/
-		tempElement = tempElement->next;
-		count++;
-	}
+  while (tempElement != NULL) {
+    printf("Zahl[%d] ist [%ld]\n", count, tempElement->primzahl);
+    /*zum nächsten Element springen*/
+    tempElement = tempElement->next;
+    count++;
+  }
 }
 
-void Cleanup()
-{
-	while(firstElement != NULL)
-	{
-		struct primzahlen *tempElement = firstElement;
-		firstElement = firstElement->next;
-		free(tempElement);
-	}
-	lastElement = firstElement = NULL;
-
+void Cleanup() {
+  while (firstElement != NULL) {
+    struct primzahlen *tempElement = firstElement;
+    firstElement = firstElement->next;
+    free(tempElement);
+  }
+  lastElement = firstElement = NULL;
 }
 
-int IstPrim(int n)
-{
-	struct primzahlen *tempElement = firstElement;
-	while(tempElement != NULL)
-	{
-		if(n % tempElement->primzahl == 0)
-		{
-			return 0;
-		}
-		tempElement = tempElement->next;
-	}
-	return 1;
+int IstPrim(int n) {
+  struct primzahlen *tempElement = firstElement;
+  while (tempElement != NULL) {
+    if (n % tempElement->primzahl == 0) {
+      return 0;
+    }
+    tempElement = tempElement->next;
+  }
+  return 1;
 }
 
-int GetAnzahlPrim()
-{
+int GetAnzahlPrim() {
 
-	if(firstElement == NULL)
-		return 0;
+  if (firstElement == NULL)
+    return 0;
 
-	int i = 0;
-	struct primzahlen *tempElement = firstElement;
-	
-	while(tempElement != NULL)
-	{
-		tempElement = tempElement->next;
-		i++;
-	}
-	
-	return i;
+  int i = 0;
+  struct primzahlen *tempElement = firstElement;
+
+  while (tempElement != NULL) {
+    tempElement = tempElement->next;
+    i++;
+  }
+
+  return i;
 }
 
-int main(void)
-{
-	int anzahlGesamt = 0;
-	AddPrimzahl(2);
+int main(void) {
+  int anzahlGesamt = 0;
+  AddPrimzahl(2);
 
-	int i = 3;
-	for(;i<=100;i++)
-	{
-		anzahlGesamt++;
-		if(IstPrim(i))
-		{
-			AddPrimzahl(i);
-			int anzPrim = GetAnzahlPrim();
-		}
-	}
+  int i = 3;
+  for (; i <= 100; i++) {
+    anzahlGesamt++;
+    if (IstPrim(i)) {
+      AddPrimzahl(i);
+      int anzPrim = GetAnzahlPrim();
+    }
+  }
 
-	/*List();*/
-	Cleanup();
+  /*List();*/
+  Cleanup();
 
-	return 0;
+  return 0;
 }
