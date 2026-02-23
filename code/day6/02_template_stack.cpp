e9644179f3b643e6798f1de53dbc2d7d46833352
m.possecker <moritzpossecker@gmail.com>
2026-02-23 12:02:04 +0100

    add second part day 6

--- code/day6/02_template_stack.cpp
+++ code/day6/02_template_stack.cpp
@@ -1,23 +1,26 @@
 #include<iostream>
+#include<string.h>
+
+using namespace std;
 
 template<class T>
-
 class container
 {
 	public:
-		T * element;
+		T element;
 		container *next;
 		container() { this->element = NULL; this->next = NULL; }
-		container(T * e) { this->element = e; this->next = NULL; } 
-}
+		container(T &e) { this->element = e; this->next = NULL; } 
+};
 
+template<class T>
 class mystack
 {
 	private:
 		container<T> *firstElement;
 	public:
 		mystack() { this->firstElement = NULL; }
-		~mystack { this->CleanUp(); }
+		~mystack() { this->CleanUp(); }
 		void Add(T e)
 		{
 			container<T> *newElement = new container<T>(e);
@@ -35,18 +38,61 @@ class mystack
 		}
 		void Print()
 		{
+			container<T> *tempElement = this->firstElement;
+			int count = 0;
+
+			while(tempElement != NULL)
+			{
+				cout << "#" << count << ": " << tempElement->element << endl;
+				count++;
+				tempElement = tempElement->next;
+			}
 
 		}
 		void CleanUp()
 		{
-
+			while (this->firstElement != NULL) this->Del();
 		}
 			
 };
 
+class student
+{
+	private:
+		char *name;
+	public:
+		student() { this->name = NULL; }
+		student(char *n)
+	       	{
+		       	this->name = new char[strlen(n) + 1];
+			strcpy(this->name, n);
+	       	}
+		~student() { delete [] this->name; }
+		friend ostream & operator << (ostream &o, const student &s)
+                {
+                        return o << s.name;
+                }
+
+};
+
 int main()
 {
+	mystack<int> m1;
+
+	m1.Add(3);
+	m1.Add(6);
+	m1.Add(7);
+	m1.Print();
+
+	m1.Del();
+	m1.Print();
+
+	mystack<student> m2;
 	
+	m2.Add(student("Maurice"));
+	m2.Print();
+	m2.Del();
+
 	return 0;
 }
 
